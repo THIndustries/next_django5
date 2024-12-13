64e88a1953f0defc3c73e96f9231d136a6d31c4b
MyName <dragon-negativ@yandex.ru>
2024-12-13 17:51:14 +0300

    Создал hr приложение

--- next_django5/settings.py
+++ next_django5/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'hr',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'next_django5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hr',
+        'USER': 'hr',
+        'PASSWORD': '321678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
