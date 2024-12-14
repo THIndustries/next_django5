9e143cee36ad201eb129746a571cbf187978f033
MyName <dragon-negativ@yandex.ru>
2024-12-15 01:09:26 +0300

    удалил поле id из hr/models Employee

--- hr/models.py
+++ hr/models.py
@@ -4,4 +4,3 @@ from django.db import models
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
