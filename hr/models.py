44b78efef75157eeee879a02c13ba49f64db86b0
MyName <dragon-negativ@yandex.ru>
2024-12-15 00:46:08 +0300

    finished employee class

--- hr/models.py
+++ hr/models.py
@@ -1,5 +1,7 @@
+import uuid
 from django.db import models
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
